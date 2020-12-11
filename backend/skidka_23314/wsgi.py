1b4b5dec202ef9da28031e1b8b6fc33c0232338b
Crowdbotics <team@crowdbotics.com>
2020-12-11 10:27:44 +0000

    Initial commit.

---
+++ backend/skidka_23314/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for skidka_23314 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skidka_23314.settings')
+
+application = get_wsgi_application()
